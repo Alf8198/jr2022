294cdd2c2f6962a84732ea2dd54b3fcc0a2fadf0
Sergey Leuhin <31506558+Alf8198@users.noreply.github.com>
2023-03-05 18:40:22 +0300

    to task 1210

---
+++ task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(4);
+        print(new Integer(5));
+    }
+
+    public static void print(int a){
+        System.out.println(a);
+    };
+    public static void print(Integer i){
+        System.out.println(i);
+    };//Напишите тут ваши методы
+}
